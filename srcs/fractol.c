34c5d7d4cd5a66b795e297344a89736115398830
Ardelle Cami <acami@mi-a3.msk.21-school.ru>
2021-06-14 21:02:04 +0300

    Moving to mlx init

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,7 +6,7 @@
 /*   By: acami <acami@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 13:47:44 by acami             #+#    #+#             */
-/*   Updated: 2021/06/14 19:51:09 by acami            ###   ########.fr       */
+/*   Updated: 2021/06/14 21:01:35 by acami            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_fractol	fractol;
 
 	fractol.fract_id = parseInput(argc, argv, &fractol);
-	//fractolInit(&fractol);
+	fractolInit(&fractol);
 	/* Draw initial pic */
 	/* Loop hook */
 	return (0);
