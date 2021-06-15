b392faf0723ae0726a298338cbf10897af266ebd
Ardelle Cami <acami@mi-m3.msk.21-school.ru>
2021-06-15 19:19:02 +0300

    Fixed a typo

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -6,7 +6,7 @@
 /*   By: acami <acami@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 13:47:44 by acami             #+#    #+#             */
-/*   Updated: 2021/06/15 19:00:57 by acami            ###   ########.fr       */
+/*   Updated: 2021/06/15 19:18:46 by acami            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ int	main(int argc, char **argv)
 
 	fractol.fract_id = parseInput(argc, argv, &fractol);
 	fractolInit(&fractol);
-	mlx_loop(fractol.mlx);
 	/* Draw initial pic */
-	/* Loop hook */
+	mlx_loop(fractol.mlx);
 	return (0);
 }
